45be885d2ed7faa64c6fc30ac373540570d98c4f
bozheng.ma1 <bozheng.ma1@cs.ucalgary.ca>
2024-09-30 22:00:53 -0600

    Initialized backend structure along with instructions

---
+++ llm-backend/workout/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create workout models here.
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    duration = models.IntegerField()
+    calories = models.IntegerField()
+
+    # user = models.ForeignKey('users.User', on_delete=models.CASCADE) # This is a placeholder for the user model that will be created later
+    
+
+    def __str__(self):
+        return self.name
