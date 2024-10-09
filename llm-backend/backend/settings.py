37a77a8a361d6b52b790fd162196e68998d61b76
Frank Ma <mabozheng@gmail.com>
2024-10-08 22:48:12 -0600

    Added new packages for Authentication

--- llm-backend/backend/settings.py
+++ llm-backend/backend/settings.py
@@ -37,10 +37,12 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
+    
     # Project apps
     'workout',
     'users',
     'llm',
+
     # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,8 +50,36 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'django.contrib.sites',
 
+    # Auth apps
+    'allauth',                                   # Allauth core
+    'allauth.account',                           # Email/password authentication
+    'allauth.socialaccount',                     # Social account handling
+    'allauth.socialaccount.providers.google',    # Google provider
+    'allauth.socialaccount.providers.facebook',  # Facebook provider
+    'allauth.socialaccount.providers.twitter',   # Twitter provider
+
+    # REST
+    'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',        
+
+
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': ['profile', 'email'],
+        'AUTH_PARAMS': {'access_type': 'offline'},
+        'CLIENT_ID': '<google-client-id>',
+        'SECRET': '<google-client-secret>',
+    }
+}
+AUTHENTICATION_BACKENDS = [
+    'allauth.account.auth_backends.AuthenticationBackend',  # django-allauth backend
+    'django.contrib.auth.backends.ModelBackend',  # Default Django auth
 ]
 
 MIDDLEWARE = [
@@ -60,6 +90,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
