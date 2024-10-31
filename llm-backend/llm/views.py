88dd84b701a1ab5c832887fcf8deb1b3691683a2
Tobin Eberle <tobineberle@gmail.com>
2024-10-31 13:12:30 -0600

    Functional LLM queries

--- llm-backend/llm/views.py
+++ llm-backend/llm/views.py
@@ -9,6 +9,9 @@ from backend.settings import API_KEY, MODEL_VERSION
 #Local imports
 from .models import LlmPrompt
 from .serializers import LlmSerializer
+from workout.models import Workout
+from workout.serializers import WorkoutSerializer
+from users.models import HealthData 
 
 # def index(request):
 #     return render(request, 'llm/')
@@ -46,41 +49,46 @@ from .serializers import LlmSerializer
 class LlmPromptView(APIView):
 
     def askLlm(self, text):
+        print("[INFO]: Getting Google Gemini Response")
         genai.configure(api_key= API_KEY)
         model = genai.GenerativeModel(MODEL_VERSION)
         response = model.generate_content(text)
-        return response.candidates[0].content.parts
-    
-    def createPrompt(self):
-        workout_text_outline = "Workout Parameters:\n"
+        print("[INFO]: Returned LLM Response:\n")
+        print("#################################")
+        print(response.candidates[0].content.parts[0].text)
+        print("#################################")
 
+        return response.candidates[0].content.parts[0].text
+    
+    def createPrompt(self, ser_obj_data):
+        print("[INFO]: Creating Prompt")
+        workout_text_outline = "Create a workout with the following parameters. Only include the workout and the workout details.\nWorkout Parameters:\n"
         #Adjust for correct model
-        for field in LlmPrompt._meta.get_fields():
+        for field in Workout._meta.get_fields():
             name = field.name
-            val = getattr(LlmPrompt.objects.last(), name)
+            #val = getattr(Workout.objects.last(), name)
+            val = ser_obj_data.get(name)
             workout_text_outline += str(name) + ": " + str(val) + "\n"
 
-        #Add routine for personal parameters
-        ######
-
-
-
-        ######
-        print(workout_text_outline)
+        # workout_text_outline += "Health Parameters:\n"
+        # for field in HealthData._meta.get_fields():
+        #     name = field.name
+        #     val = getattr(HealthData.objects.last(), name)
+        #     workout_text_outline += str(name) + ": " + str(val) + "\n"
+        workout_text_outline += "\nReturn your response in pure text format using only new line delimiters with no markdown styling."
         return workout_text_outline
     
     def get(self, request, format = None):
-        model_data = LlmPrompt.objects.all()
-        serializer = LlmSerializer(model_data, many = True)
+        model_data = Workout.objects.all()
+        serializer = WorkoutSerializer(model_data, many = True)
         return Response(serializer.data)
 
     def post(self, request, format = None):
-        serializer = LlmSerializer(data = request.data)
-        #Change var as needed
-        prompt_text = self.createPrompt()
+        serializer = WorkoutSerializer(data = request.data)
         if serializer.is_valid():
-            text = serializer.validated_data.get('prompt_text')
-            #serializer.save(response_text = self.askLlm(text))
+            prompt_text = self.createPrompt(serializer.validated_data)
+            response_text = self.askLlm(prompt_text)
+            serializer.save(llm_final_workout = response_text)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
