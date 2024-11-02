a33d886d8f3212ab2a44808fbf30ceebccf9086f
jeffwheeler257 <jeff.wheeler@ucalgary.ca>
2024-11-02 14:42:44 -0600

    Workout test (#13)
    Updated workout

--- llm-backend/workout/urls.py
+++ llm-backend/workout/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CreateWorkoutView.as_view(), name='main-workout'),
-    path('<str:user>/list/', views.WorkoutListView.as_view(), name='workout-list'),
-    path('<str:user>/<int:id>/', views.WorkoutView.as_view(), name='specific-workout')
+    path('list/', views.WorkoutListView.as_view(), name='workout-list'),
+    path('<int:id>/', views.WorkoutView.as_view(), name='specific-workout')
 ]
