afa92d553cc878c870efcaa45330b89fb6119ab8
Tobin Eberle <52223659+tobineberle@users.noreply.github.com>
2024-11-05 21:22:36 -0700

    40 ll mcreate workout view (#17)
    * Update workout models, serializers, urls, views to add CreateWorkoutView
    
    * Update workout models and serializer, work on CreateWorkoutView and ModifyWorkoutView
    
    * Test create workout on postman, minor fixes
    
    * llm work
    
    * Minor updates
    
    * Populate workout model with new fields. Update serializer and views.
    
    * LLM Updates
    
    * Added create workout method
    
    * Functional LLM queries
    
    * modified workout with llm changes
    
    * added prompts.py
    
    ---------
    
    Co-authored-by: Jeff Wheeler <jeff.wheeler@ucalgary.ca>

--- llm-backend/workout/views.py
+++ llm-backend/workout/views.py
@@ -8,6 +8,12 @@ import requests
 from rest_framework.permissions import IsAuthenticated
 from django.core.exceptions import ObjectDoesNotExist
 
+#For llm prompting
+from backend.settings import API_KEY, MODEL_VERSION
+from .prompts import prompt_start, prompt_end
+import google.generativeai as genai
+
+
 class CreateWorkoutView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -15,6 +21,11 @@ class CreateWorkoutView(APIView):
     def post(self, request):
         serializer = WorkoutSerializer(data=request.data)
         if serializer.is_valid():
+            try:
+                llm = LlmConnection()
+                llm.requestWorkout(serializer)
+            except:
+                print("[ERROR]: LLM Request Failed")
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -51,3 +62,40 @@ class WorkoutView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Workout.DoesNotExist:
             return Response({"error": "Workout not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+
+''' Used to connect and query llm'''
+class LlmConnection():
+
+    api_key = API_KEY
+    model_version = MODEL_VERSION
+    model = genai.GenerativeModel(model_version)
+
+    def __init__(self):
+        genai.configure(api_key = self.api_key)
+        return
+
+    '''Request a workout from the llm'''
+    def requestWorkout(self, serializer):
+        print("[INFO]: Connecting to Gemini")
+        prompt = self.generatePrompt(serializer.validated_data)
+        response = self.model.generate_content(prompt)
+        return response.candidates[0].content.parts[0].text
+    
+    '''Make changes to the current llm workout'''
+    def changeWorkout(self, serializer):
+        prompt = self.generatePrompt(serializer.validated_data)
+        return
+    
+    '''Generates llm prompts'''
+    def generatePrompt(self, serial_val_data):
+        print("[INFO]: Creating Prompt")
+        prompt = prompt_start
+        for field in Workout._meta.get_fields():
+            name = field.name
+            val = serial_val_data.get(name)
+            prompt += str(name) + ": " + str(val) + "\n"
+        prompt += prompt_end
+        return prompt
+
+
