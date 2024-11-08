20af4b6e29b47f7f7f2691b8bfb69acfdb6deef6
jeffwheeler257 <jeff.wheeler@ucalgary.ca>
2024-11-08 16:20:40 -0700

    Workout patch updates (#35)
    * Change logic in WorkoutView patch so calls to the llm are only made if the llm_suggested_changes field is altered. Removed llm_final_workout from workout model.
    
    * Update workout serializer with actual_length field.
    
    * Start creating tests for workout views.
    
    * Work on workout tests.
    
    * Work on tests.

--- llm-backend/workout/urls.py
+++ llm-backend/workout/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CreateWorkoutView.as_view(), name='main-workout'),
+    path('', views.CreateWorkoutView.as_view(), name='create-workout'),
     path('list/', views.WorkoutListView.as_view(), name='workout-list'),
     path('<int:id>/', views.WorkoutView.as_view(), name='specific-workout')
 ]
